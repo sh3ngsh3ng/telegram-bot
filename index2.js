f93a23df2c6829c5689bb13383e931c209279ad0
ys <ysdeveloper01@gmail.com>
2022-06-06 10:26:50 +0800

    ported bot functions to webhook version

--- index2.js
+++ index2.js
@@ -40,6 +40,46 @@ async function main() {
         })
     })
 
+    bot.on('message', async (msg) => {
+        // console.log("Message send =>", msg)
+        var Hi = "hi";
+        if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+            bot.sendMessage(msg.chat.id,"Hello " + msg.from.first_name);
+        }
+
+        if (msg.text == "1) Get Homework List") {
+            bot.sendMessage(msg.chat.id, "This is the list of your homework...")
+
+        }
+
+        // get chat id
+        if (msg.text == "chatid") {
+            bot.sendMessage(msg.chat.id, msg.chat.id)
+        }
+
+        // get chat number count
+        if (msg.text == "count") {
+            let count = await bot.getChatMemberCount(msg.chat.id)
+            bot.sendMessage(msg.chat.id, count)
+        }
+
+
+        // send poll
+        let question = "heelllo??"
+        let options = ["1", "2", '3']
+
+        if (msg.text == "poll") {
+            let result = bot.sendPoll(msg.chat.id, question, options)
+            console.log(result)
+        }
+
+        //send dice?
+        if (msg.text =="dice") {
+            bot.sendDice(msg.chat.id)
+        }
+
+    })
+
 }
 
 
