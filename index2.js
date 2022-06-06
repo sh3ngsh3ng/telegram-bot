55ab4a54bd4e3add92220fad13e6b4347d595c57
ys <ysdeveloper01@gmail.com>
2022-06-06 10:01:45 +0800

    trying webhook

---
+++ index2.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const TelegramBot = require('node-telegram-bot-api')
+let app = express()
+
+require('dotenv').config()
+
+const token = process.env.API_KEY
+const port = process.env.PORT || 8080
+
+app.use(express.json())
+
+
+
+async function main() {
+    app.get("/", (req,res) => {
+        res.status(200).json({
+            "message": "Hello! The bot is working!"
+        })
+    })
+    
+    app.post(`/${process.env.TELEGRAM_TOKEN}`, (req,res) => {
+        bot.processUpdate(req.body)
+    })
+
+
+    bot.onText(/\/start/, (msg) => {
+        bot.sendMessage(msg.chat.id, "Jarrvee at yourrr serviceeee. What would you like to do, " + msg.from.first_name + " ?", {
+            "reply_markup": {
+                "keyboard": [
+                    ["1) Get Homework List"],
+                    ["KILL BOT"]
+                ]
+            }
+        })
+    })
+
+}
+
+
+main()
+
+
+app.listen(port, () => {
+    console.log("Server started")
+})
+
+
+
